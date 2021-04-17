f0aa6f381abc83c4c18ffe87095c5c752b876a0f
Horácio Comé <horaciojr001@gmail.com>
2021-04-17 23:10:34 +0200

    set up navigation flow

---
+++ app/src/main/java/io/github/horaciocome1/witweather/CityWeatherViewModel.kt
@@ -0,0 +1,7 @@
+package io.github.horaciocome1.witweather
+
+import androidx.lifecycle.ViewModel
+
+class CityWeatherViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
---
+++ app/src/main/java/io/github/horaciocome1/witweather/HomeViewModel.kt
@@ -0,0 +1,7 @@
+package io.github.horaciocome1.witweather
+
+import androidx.lifecycle.ViewModel
+
+class HomeViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
