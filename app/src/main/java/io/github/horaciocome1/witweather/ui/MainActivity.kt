c5a593dfee9dcb8955d2fb8f519084cdfa4c58af
Horácio Comé <horaciojr001@gmail.com>
2021-04-22 17:54:11 +0200

    reviewed activities reviewed ui

--- app/src/main/java/io/github/horaciocome1/witweather/ui/MainActivity.kt
+++ app/src/main/java/io/github/horaciocome1/witweather/ui/MainActivity.kt
@@ -9,22 +9,13 @@ import android.util.Log
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
-import androidx.lifecycle.lifecycleScope
 import com.google.android.gms.location.*
-import com.google.android.material.snackbar.Snackbar
-import io.github.horaciocome1.witweather.R
 import io.github.horaciocome1.witweather.databinding.ActivityMainBinding
 import io.github.horaciocome1.witweather.ui.home.HomeSharedViewModel
 import io.github.horaciocome1.witweather.util.Constants
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.tasks.await
 
 
 class MainActivity : AppCompatActivity() {
-
-    companion object {
-        private const val TAG = "MainActivity"
-    }
 
     private lateinit var binding: ActivityMainBinding
 
@@ -32,19 +23,6 @@ class MainActivity : AppCompatActivity() {
 
     private val fusedLocationProviderClient: FusedLocationProviderClient by lazy {
         LocationServices.getFusedLocationProviderClient(this)
-    }
-
-    private val snackbarError: Snackbar by lazy {
-        Snackbar.make(binding.root, getString(R.string.location_error), Snackbar.LENGTH_INDEFINITE)
-                .setAction(getString(R.string.retry)) {
-                    snackbarError.dismiss()
-                    snackbarLoading.show()
-                    resetLocationCallback()
-                }
-    }
-
-    private val snackbarLoading: Snackbar by lazy {
-        Snackbar.make(binding.root, getString(R.string.location_loading), Snackbar.LENGTH_SHORT)
     }
 
     private val locationRequest: LocationRequest by lazy {
@@ -59,11 +37,7 @@ class MainActivity : AppCompatActivity() {
             override fun onLocationResult(locationResult: LocationResult?) {
                 locationResult ?: return
                 for (location in locationResult.locations) {
-                    if (location.latitude == 0.0 && location.longitude == 0.0) {
-                        snackbarError.show()
-                    } else {
-                        sharedViewModel.setGeoCoordinates(location.latitude, location.longitude)
-                    }
+                    sharedViewModel.setGeoCoordinates(location.latitude, location.longitude)
                 }
             }
         }
@@ -78,12 +52,9 @@ class MainActivity : AppCompatActivity() {
     override fun onStart() {
         super.onStart()
         managePermissionRequests()
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            sharedViewModel.setLocationPermissionGranted(false)
-            return
+        if (isPermissionGranted()) {
+            monitorLocationChanges()
         }
-        sharedViewModel.setLocationPermissionGranted(true)
-        fusedLocationProviderClient.requestLocationUpdates(locationRequest, locationCallback, Looper.getMainLooper())
     }
 
     override fun onPause() {
@@ -97,16 +68,13 @@ class MainActivity : AppCompatActivity() {
             grantResults: IntArray
     ) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        Log.d("ActivityMain", "onRequestPermissionsResult: $requestCode\n$permissions\n$grantResults")
         when (requestCode) {
             Constants.REQUEST_CODE_LOCATION_PERMISSIONS -> {
                 val isGranted = grantResults.first() == PackageManager.PERMISSION_GRANTED
                 if (isGranted) {
-                    Log.d("ActivityMain", "onRequestPermissionsResult: GRANTED")
                     sharedViewModel.setLocationPermissionGranted(true)
                     return
                 }
-                Log.d("ActivityMain", "onRequestPermissionsResult: DENIED")
                 sharedViewModel.setLocationPermissionGranted(false)
             }
         }
@@ -114,7 +82,6 @@ class MainActivity : AppCompatActivity() {
 
     private fun managePermissionRequests() {
         sharedViewModel.isLocationPermissionRequested.observe(this) {
-            Log.d("ActivityMain", "onStart: $it")
             if (it) {
                 ActivityCompat.requestPermissions(
                         this,
@@ -125,10 +92,17 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    private fun isPermissionGranted(): Boolean {
+        val isFineLocationGranted = ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+        val isCoarseLocationGranted = ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED
+        val isPermissionGranted = isFineLocationGranted && isCoarseLocationGranted
+        sharedViewModel.setLocationPermissionGranted(isPermissionGranted)
+        return isPermissionGranted
+    }
+
     @SuppressLint("MissingPermission")
-    private fun resetLocationCallback() {
+    private fun monitorLocationChanges() {
         fusedLocationProviderClient.requestLocationUpdates(locationRequest, locationCallback, Looper.getMainLooper())
-        fusedLocationProviderClient.removeLocationUpdates(locationCallback)
     }
 
 }
